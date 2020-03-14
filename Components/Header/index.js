18ac6a4816f446e00c928be856fa19d4dd8ea368
Naveed Elahi <naveed@isuntv.com>
2020-03-14 10:48:23 +0800

    Create icon for PlayerItem

--- Components/Header/index.js
+++ Components/Header/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import View from "react-native-web/dist/exports/View";
-import styles from "./Styles";
+import styles from "./styles";
 import Text from "react-native-web/dist/exports/Text";
 
 const Header = () => {
     return (
         <View style={styles.container}>
-            <Text>Player 1</Text>
-            <Text>Player 2</Text>
+            <View>
+                <View style={[styles.icon, styles.player1]} />
+                <Text>Player 1</Text>
+            </View>
+            <View>
+                <View style={[styles.icon, styles.player2]} />
+                <Text>Player 2</Text>
+            </View>
         </View>
     )
 
