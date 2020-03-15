127fe85d270f566c305ecc847c001d386835a812
Naveed Elahi <naveed@isuntv.com>
2020-03-15 09:04:31 +0800

    Create logic to initialize the state of the board

--- Components/Grid/index.js
+++ Components/Grid/index.js
@@ -3,8 +3,21 @@ import styles from "./styles";
 import {View} from 'react-native';
 import Item from "./Item";
 
+const COLUMNS = 7
+const ROWS = 6
 const COLUMN_INDICES = [0,1,2,3,4,5,6]
 const ROW_INDICES = [5,4,3,2,1,0]
+
+const initialBoardState = () => {
+    const matrix = []
+    for(let i = 0; i < COLUMNS; i++) {
+        matrix.push([])
+        for(let j = 0; j < ROWS; j++) {
+            matrix[i].push(null)
+        }
+    }
+    return matrix
+}
 
 const Grid = () => {
     return (
